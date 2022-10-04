cac60b48a929eda7692cd1becb710a77178fa492
patrik <patrik.ellini@outlook.com>
2022-10-04 12:29:58 +0100

    redered the contact list

--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -7,25 +7,40 @@ function ContactsAdd(props) {
   // to this component so new contacts can be added to the
   // state
   const { setContacts, contacts } = props
+  const url = 'http://localhost:4000/contacts'
 
   //TODO: Implement controlled form
   //send POST to json server on form submit
+  const onSubmitt = (e) => {
+    e.preventDefault()
+    console.log(e)
+    fetch(url, {
+   method: 'POST',
+   headers: {
+       'Content-Type': 'application/json'
+   },
+   body: JSON.stringify({
+    contacts 
+   })
+})
 
+  }
   return (
-    <form className="form-stack contact-form">
+    <form className="form-stack contact-form" onSubmit={onSubmitt}>
       <h2>Create Contact</h2>
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+    
+      <input id="firstName" name="firstName" type="text" required onChange={e => setContacts({...contacts, firstName: e.target.value})}/>
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input id="lastName" name="lastName" type="text" required onChange={e => setContacts({...contacts, lastName: e.target.value})}/>
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input id="street" name="street" type="text" required onChange={e => setContacts({...contacts, street: e.target.value})}/>
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input id="city" name="city" type="text" required onChange={e => setContacts({...contacts, city: e.target.value})}/>
 
       <div className="actions-section">
         <button className="button blue" type="submit">
